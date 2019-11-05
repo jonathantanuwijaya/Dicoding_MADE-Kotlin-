d3e3366c3740ca00879b1be2d718539d1f265502
Jonathan Tanuwijaya <jonathantw64@gmail.com>
2019-11-05 22:54:12 +0700

    submission 2 done

--- app/src/main/java/com/example/submission1/Fragment/MoviesFragment.kt
+++ app/src/main/java/com/example/submission1/Fragment/MoviesFragment.kt
@@ -1,8 +1,8 @@
 package com.example.submission1.Fragment
 
-import android.content.Context
+
 import android.content.res.TypedArray
-import android.net.Uri
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -15,30 +15,15 @@ import com.example.submission1.R
 import kotlinx.android.synthetic.main.fragment_movies.*
 
 
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [MoviesFragment.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [MoviesFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 
 class MoviesFragment : Fragment() {
-    // TODO: Rename and change types of parameters
 
-    private var listener: OnFragmentInteractionListener? = null
     private lateinit var judulFilm: Array<String>
     private lateinit var descFilm: Array<String>
     private lateinit var foto: TypedArray
     private var movies = arrayListOf<FilmMovie>()
     private var adaptermovie = MovieAdapter(movies)
 
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//
-//
-//    }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
@@ -81,38 +66,7 @@ class MoviesFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_movies, container, false)
     }
-//
-//    // TODO: Rename method, update argument and hook method into UI event
-//
-//    override fun onAttach(context: Context) {
-//        super.onAttach(context)
-//        if (context is OnFragmentInteractionListener) {
-//            listener = context
-//        } else {
-//            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
-//        }
-//    }
-//
-//    override fun onDetach() {
-//        super.onDetach()
-//        listener = null
-//    }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson [Communicating with Other Fragments]
-     * (http://developer.android.com/training/basics/fragments/communicating.html)
-     * for more information.
-     */
-    interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onFragmentInteraction(uri: Uri)
-    }
 
     companion object {
         /**
--- app/src/main/java/com/example/submission1/Fragment/TVShowFragment.kt
+++ app/src/main/java/com/example/submission1/Fragment/TVShowFragment.kt
@@ -1,15 +1,12 @@
 package com.example.submission1.Fragment
 
-import android.content.Context
 import android.content.res.TypedArray
-import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.submission1.Adapter.MovieAdapter
 import com.example.submission1.Adapter.TVShowAdapter
 import com.example.submission1.Model.FilmMovie
 import com.example.submission1.R
@@ -17,8 +14,6 @@ import kotlinx.android.synthetic.main.fragment_tvshow.*
 
 class TVShowFragment : Fragment() {
 
-    private var listener: OnFragmentInteractionListener? = null
-//    private var listener: MoviesFragment.OnFragmentInteractionListener? = null
     private lateinit var judulFilm: Array<String>
     private lateinit var descFilm: Array<String>
     private lateinit var foto: TypedArray
@@ -42,10 +37,6 @@ class TVShowFragment : Fragment() {
         }
         return listmovies
     }
-
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//    }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
@@ -71,37 +62,6 @@ class TVShowFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_tvshow, container, false)
-    }
-
-//
-//    override fun onAttach(context: Context) {
-//        super.onAttach(context)
-//        if (context is OnFragmentInteractionListener) {
-//            listener = context
-//        } else {
-//            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
-//        }
-//    }
-//
-//    override fun onDetach() {
-//        super.onDetach()
-//        listener = null
-//    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson [Communicating with Other Fragments]
-     * (http://developer.android.com/training/basics/fragments/communicating.html)
-     * for more information.
-     */
-    interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onFragmentInteraction(uri: Uri)
     }
 
     companion object {
